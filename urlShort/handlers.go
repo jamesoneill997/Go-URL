f886a5d3981bf49011b2196d078b68c9722a0a4e
jamesoneill997 <jamesoneill997@gmail.com>
2020-11-21 15:50:39 +0000

    added handlers

--- urlShort/handlers.go
+++ urlShort/handlers.go
@@ -8,30 +8,45 @@ import (
 // implements http.Handler) that will attempt to map any
 // paths (keys in the map) to their corresponding URL (values
 // that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r http.Request) {
+		p := r.URL.Path
+
+		path, ok := pathsToUrls[p]
+
+		if ok {
+			http.Redirect(w, r, path, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
 // an http.HandlerFunc (which also implements http.Handler)
 // that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+// URL.
+func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var unmYAML []map[string]string
+
+	e := yamlV2.Unmarshal(yaml, &unmYAML)
+
+	//if yaml is of invalid format
+	if e != nil {
+		return nil, err
+	}
+
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func buildMap(parsedYaml []map[string]string) map[string]string {
+
+	pathURLMap := make(map[string]string)
+
+	for _, entry := range parsedYaml {
+		key := entry["path"]
+		pathUrlMap[key] = entry["url"]
+	}
+	return pathURLMap
 }
